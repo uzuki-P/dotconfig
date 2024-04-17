cc60ff03382133111a2fc6a18b5424ad88ee74c6
Uzuki-P <uzuki-P@users.noreply.github.com>
2024-04-17 22:58:33 +0700

    update

--- .config/diy-kde-bar/widget.sh
+++ .config/diy-kde-bar/widget.sh
@@ -6,10 +6,12 @@ while true
 do
   MEM=$(/home/uzuki_p/.config/diy-kde-bar/script/memory.sh)
   TEMP=$(sensors | grep "^[ ]*CPU" | awk '{print substr($2,2)}')
+  DISK=$(df -h -P "$HOME" | awk '/\/.*/ { print $5 }')
   
   DATA=""
   DATA+="| A | $MEM | RAM Usage | |"
-  DATA+="| B | $TEMP | CPU Temp | |"
+  DATA+="| A | $TEMP | CPU Temp | |"
+  DATA+="| A | $DISK | Home Storage Used | |"
 
   qdbus org.kde.plasma.doityourselfbar /id_$ID \
     org.kde.plasma.doityourselfbar.pass "$DATA"
