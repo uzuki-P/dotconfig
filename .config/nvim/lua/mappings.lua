538784795119266ccc6dd53b8433b0ebc466144f
Uzuki-P <uzuki-P@users.noreply.github.com>
2024-04-19 13:55:33 +0700

    update

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -8,5 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<A-1>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
 map({"n", "v"}, "<leader>y", "\"+y", { desc = "Copy to clipboard" })
 
+map("n", "<C-A-S>", "<cmd>w !sudo tee %<CR>", { desc = "Nvimtree Toggle window" })
+
 -- map("i", "jk", "<ESC>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
