f34d442e526763de82a0e97badaaf845da0052bc
Uzuki-P <uzuki-P@users.noreply.github.com>
2025-01-16 18:26:17 +0700

    cleanup fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,24 @@
-# Custom sourcing of colors, exports, paths, grc, multi-function fish files, etc.
 source ~/.config/fish/conf.d/abbr.fish
-#source ~/.config/fish/conf.d/colors.fish # this could be obsolete by starship & eza
 source ~/.config/fish/conf.d/paths.fish
+
+function fish_greeting
+    switch (random 1 5)
+        case 1
+            echo "🐟"
+        case 2
+            echo "🐠"
+        case 3
+            echo "🐡"
+        case 4
+            echo "🐳"
+        case 5
+            echo "🦈"
+    end
+end
 
 # starship. https://starship.rs/
 function starship_transient_rprompt_func
-  starship module time
+    starship module time
 end
 starship init fish | source
 enable_transience
@@ -15,26 +28,25 @@ zoxide init --cmd cd fish | source
 
 # setup yazi. https://yazi-rs.github.io/docs/quick-start#shell-wrapper
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
 end
 
 # pnpm. 
 set -gx PNPM_HOME "/home/uzuki_p/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 
 # Generated for envman. Do not edit.
-test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
+# test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
 
 # Carapace. https://carapace-sh.github.io/carapace-bin/spec/bridge.html?highlight=fish#fish
-set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
-mkdir -p ~/.config/fish/completions
-carapace --list | awk '{print $1}' | xargs -I{} touch ~/.config/fish/completions/{}.fish # disable auto-loaded completions (#185)
+#set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
+#mkdir -p ~/.config/fish/completions
+#carapace --list | awk "{print $1}" | xargs -I{} touch ~/.config/fish/completions/{}.fish # disable auto-loaded completions (#185)
 carapace _carapace | source
-
