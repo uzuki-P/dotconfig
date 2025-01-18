5890d563c38ab8a433648c3bedb50c87fb858949
uzuki-P <rahmatfajar15@gmail.com>
2025-01-18 15:09:28 +0700

    update readme & some nvim config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 source ~/.config/fish/conf.d/abbr.fish
 source ~/.config/fish/conf.d/paths.fish
 
+set -g fish_case_sensitive_suggestions false
+
 function fish_greeting
     switch (random 1 5)
         case 1
@@ -21,7 +23,7 @@ function starship_transient_rprompt_func
     starship module time
 end
 starship init fish | source
-enable_transience
+#enable_transience
 
 # zoxide. https://github.com/ajeetdsouza/zoxide
 zoxide init --cmd cd fish | source
@@ -42,11 +44,5 @@ if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
 
-# Generated for envman. Do not edit.
-# test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
-
 # Carapace. https://carapace-sh.github.io/carapace-bin/spec/bridge.html?highlight=fish#fish
-#set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
-#mkdir -p ~/.config/fish/completions
-#carapace --list | awk '{print $1}' | xargs -I{} touch ~/.config/fish/completions/{}.fish # disable auto-loaded completions (#185)
 carapace _carapace | source
