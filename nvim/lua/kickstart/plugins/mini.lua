9ad910b62bfffd151a6a6ce9f22ee190067830df
Uzuki-P <uzuki-P@users.noreply.github.com>
2025-01-18 15:51:31 +0700

    update and fix paste on nvim

--- nvim/lua/kickstart/plugins/mini.lua
+++ nvim/lua/kickstart/plugins/mini.lua
@@ -32,6 +32,8 @@ return {
         return '%2l:%-2v'
       end
 
+      require('mini.pairs').setup()
+
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
