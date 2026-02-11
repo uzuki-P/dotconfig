9081e0c3c04eeee8d303d939352f32cb8361d4e6
Uzuki-P <uzuki-P@users.noreply.github.com>
2026-02-11 10:04:49 +0700

    update after fedora 43

--- fish/conf.d/paths.fish
+++ fish/conf.d/paths.fish
@@ -19,9 +19,10 @@ fish_add_path -ga ~/.puro/shared/pub_cache/bin
 fish_add_path -ga ~/.puro/envs/default/flutter/bin
 fish_add_path -ga ~/.npm-global/bin
 fish_add_path -ga ~/.maestro/bin
-fish_add_path -ga ~/apps/scrcpy # make sure below android/tools 
+fish_add_path -ga ~/apps/scrcpy # make sure below android/tools
 fish_add_path -ga ~/.opencode/bin
 fish_add_path -ga ~/.bun/bin
+fish_add_path -ga ~/.local/share/fnm
 
 ## for bootdev https://github.com/bootdotdev/bootdev?tab=readme-ov-file#1-install-go-122-or-later
 fish_add_path -ga ~/.local/opt/go/bin
