67347a9fb7cbccecb577144019130d8791839d3e
Uzuki-P <uzuki-P@users.noreply.github.com>
2026-06-09 15:28:36 +0700

    chore: update zed config & fish path

--- fish/conf.d/paths.fish
+++ fish/conf.d/paths.fish
@@ -3,11 +3,11 @@ set -e fish_user_paths
 
 ## ADD to PATH ##
 #fish_add_path -ga ~/sdk/flutter/bin
-#fish_add_path -ga /home/linuxbrew/.linuxbrew/bin
+fish_add_path -ga /home/linuxbrew/.linuxbrew/bin
 fish_add_path -ga ~/go/bin
-#fish_add_path -ga ~/Android/Sdk/tools
-#fish_add_path -ga ~/Android/Sdk/tools/bin
-#fish_add_path -ga ~/Android/Sdk/platform-tools
+fish_add_path -ga ~/Android/Sdk/tools
+fish_add_path -ga ~/Android/Sdk/tools/bin
+fish_add_path -ga ~/Android/Sdk/platform-tools
 fish_add_path -ga ~/script
 fish_add_path -ga ~/bin
 #fish_add_path -ga ~/.config/composer/vendor/bin
@@ -33,8 +33,8 @@ set -gx EDITOR nvim
 set -gx MANPATH /usr/local/man
 # set -gx JAVA_HOME /usr/lib/jvm/temurin-17-jdk
 #set -gx JAVA_HOME /home/uzuki_p/.jdks/jbr-21.0.10
-#set -gx ANDROID_HOME $HOME/Android/Sdk
-#set -gx ANDROID_SDK $HOME/Android/Sdk
+set -gx ANDROID_HOME $HOME/Android/Sdk
+set -gx ANDROID_SDK $HOME/Android/Sdk
 #set -gx PURO_ROOT $HOME/.puro
 #set -gx PUB_CACHE $HOME/.puro/shared/pub_cache
 #set -gx BUN_INSTALL $HOME/.bun
